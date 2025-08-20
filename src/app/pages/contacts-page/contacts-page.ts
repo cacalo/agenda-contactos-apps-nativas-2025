f33c175973568b1528af521738974ef3710dd8d3
cacalo <cacalo219@hotmail.com>
2025-08-20 11:40:02 -0300

    Creación de componentes básicos y ruteo básico

---
+++ src/app/pages/contacts-page/contacts-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-contacts-page',
+  imports: [RouterModule],
+  templateUrl: './contacts-page.html',
+  styleUrl: './contacts-page.scss'
+})
+export class ContactsPage {
+  
+}
---
+++ src/app/pages/login-page/login-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [RouterModule],
+  templateUrl: './login-page.html',
+  styleUrl: './login-page.scss'
+})
+export class LoginPage {
+
+}
