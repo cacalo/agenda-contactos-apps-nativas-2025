2c27441ac7c8338a29356bcd71ae3c443b3ce157
cacalo <cacalo219@hotmail.com>
2025-08-27 11:15:29 -0300

    Implementación de for en template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,26 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
 import { ContactsPage } from './pages/contacts-page/contacts-page';
 import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
+import { LoggedLayout } from './layouts/logged-layout/logged-layout';
 
 export const routes: Routes = [
-    {
-        path: "login",
-        component: LoginPage
-    },
-    {
+  {
+    path: "login",
+    component: LoginPage
+  },
+  {
+    path: "",
+    component: LoggedLayout,
+    children: [
+      {
         path: "",
         component: ContactsPage
-    },
-    {
+      },
+      {
         path: "contacts/:id",
         component: ContactDetailsPage
-    },
+      },
+    ]
+  },
+
 ];
--- src/app/pages/contacts-page/contacts-page.ts
+++ src/app/pages/contacts-page/contacts-page.ts
@@ -8,5 +8,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './contacts-page.scss'
 })
 export class ContactsPage {
-  
+
+  contactos = [
+    {
+      nombre: 'Gonzalo',
+      telefono: 3476123123,
+      email: 'gbechara@austral.edu.ar'
+    },
+    {
+      nombre: 'Lucho',
+      telefono: 3471234234,
+      email: 'lbarletta@austral.edu.ar'
+    }
+  ]
+
 }
