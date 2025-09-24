5dffdb0a505ec830d20235021d86d1eae1629f5e
Cristián <crgallo@frro.utn.edu.ar>
2025-09-24 11:59:17 -0300

    Edición de contactos funcionando

--- src/app/pages/new-edit-contact/new-edit-contact.ts
+++ src/app/pages/new-edit-contact/new-edit-contact.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, inject, input, OnInit, viewChild } from '@angular/core';
-import { Form, FormsModule, NgForm } from '@angular/forms';
+import { Form, FormGroup, FormsModule, NgControl, NgForm, NgModel } from '@angular/forms';
 import { Contact, NewContact } from '../../interfaces/contact';
 import { ContactsService } from '../../services/contacts-service';
 import { Router } from '@angular/router';
@@ -11,22 +11,33 @@ import { Router } from '@angular/router';
   styleUrl: './new-edit-contact.scss'
 })
 export class NewEditContact implements OnInit {
-  contactsService = inject(ContactsService);
-  router = inject(Router)
+   contactsService = inject(ContactsService);
+   router = inject(Router)
   errorEnBack = false;
-  idContacto = input<number>();
+   idContacto = input<number>();
   contactoOriginal:Contact|undefined = undefined;
-  form = viewChild<ElementRef<Form>>('newContactForm');
-
+   form = viewChild<NgForm>('newContactForm');
   
   async ngOnInit() {
     if(this.idContacto()){
       this.contactoOriginal = await this.contactsService.getContactById(this.idContacto()!);
-      console.log(this.contactoOriginal)
+      // Cambio los valores del formulario
+      this.form()?.setValue({
+        firstName: this.contactoOriginal!.firstName,
+        lastName: this.contactoOriginal!.lastName,
+        address: this.contactoOriginal!.address,
+        email: this.contactoOriginal!.email,
+        image: this.contactoOriginal!.image,
+        number: this.contactoOriginal!.number,
+        company: this.contactoOriginal!.company,
+        isFavourite: this.contactoOriginal!.isFavorite
+      })
     }
   }
 
-  async createContact(form:NgForm){
+  /** Revisa si estamos editando o creando un contacto y ejecuta la función correspondiente del servicio de contactos */
+  async handleFormSubmission(form:NgForm){
+
     this.errorEnBack = false;
     const nuevoContacto: NewContact ={
       firstName: form.value.firstName,
@@ -38,12 +49,19 @@ export class NewEditContact implements OnInit {
       company: form.value.company,
       isFavorite: form.value.isFavorite
     }
+    let res;
+    // const res = await this.contactsService.createContact(nuevoContacto);
+    if(this.idContacto()){
+      res = await this.contactsService.editContact({...nuevoContacto,id:this.idContacto()!.toString()})
+    } else {
+      res = await this.contactsService.createContact(nuevoContacto);
+    }
 
-    const res = await this.contactsService.createContact(nuevoContacto);
     if(!res) {
       this.errorEnBack = true;
       return
     };
     this.router.navigate(["/contacts",res.id]);
   }
+
 }
